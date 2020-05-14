7f3be4373530454c9fc0adc104726f739fd7ef7d
Andrew Chambers <ac@acha.ninja>
2020-05-14 15:19:08 +1200

    Update dev env for new hermes syntax.

--- support/dev-shell.sh
+++ support/dev-shell.sh
@@ -7,6 +7,8 @@ unset JANET_MODPATH JANET_HEADERPATH
 export JANET_LIBPATH="$(realpath $(dirname $(realpath ./dev-env/bin/janet))/../lib)"
 export ORIG_PATH=$PATH
 . ./dev-env/activate
+export JANET_LIBPATH="$(dirname $(which janet))/../lib"
+export JANET_HEADERPATH="$(dirname $(which janet))/../include/janet"
 export PATH=$PATH:$ORIG_PATH
 export HERMES_STATIC_BUILD=yes
 exec /bin/sh --norc --noprofile "$@"
